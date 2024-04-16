89107a6da7356a7f75809d423f9d4c89c64a227c
iamorales1 <iamorales1@mail.csuchico.edu>
2024-04-15 22:19:46 -0700

    started on observer pattern

---
+++ CAGD380_Gauntlet/Assets/_Scripts/Observer/CameraController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : Observer
+{
+    private bool _hitOtherPlayer;
+    private bool _hitFood;
+    private bool _lifeForceLow;
+    private Vector3 _initialPosition;
+    //private float _shakeMagnitude = 0.1f;
+    private PlayerController _playerController;
+
+    private void OnEnable()
+    {
+        _initialPosition = gameObject.transform.localPosition;
+    }
+
+    /// <summary>
+    /// Simple BAD version of screenshake
+    /// IEnumerator is better or some kind of coroutine
+    /// </summary>
+    private void Update()
+    {
+        if (_lifeForceLow)
+        {
+            gameObject.transform.localPosition = _initialPosition + (Random.insideUnitSphere * _shakeMagnitude);
+        }
+        else
+        {
+            gameObject.transform.localPosition = _initialPosition;
+        }
+
+
+    }
+    /// <summary>
+    /// All we do here in Notify is get the reference to the BikeController
+    /// and thenset our local reference to the _isTurboOn
+    /// The actual screenshake is turned on/off thru Update
+    /// </summary>
+    /// <param name="subject"></param>
+    public override void Notify(Subject subject)
+    {
+        if (!_playerController)
+        {
+            _playerController = subject.GetComponent<PlayerController>();
+        }
+        if (_playerController)
+        {
+            _lifeForceLow = _playerController.IsTurboOn;
+        }
+    }
+}
---
+++ CAGD380_Gauntlet/Assets/_Scripts/Observer/HUDController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDController : Observer
+{
+    private bool _hitOtherPlayer;
+    private bool _hitFood;
+    private bool _lifeForceLow;
+
+    private bool _isTurboOn;
+    private float _currentHealth;
+    private PlayerController _playerController;
+
+    private void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(50, 50, 100, 200));
+        GUILayout.BeginHorizontal("box");
+        GUILayout.Label("Health" + _currentHealth);
+        GUILayout.EndHorizontal();
+
+        if (_lifeForceLow)
+        {
+            GUILayout.BeginHorizontal("box");
+            GUILayout.Label("WARNING LOW HEALTH");
+            GUILayout.EndHorizontal();
+        }
+
+        GUILayout.EndArea();
+    }
+
+    /// <summary>
+    /// receive a reference to subject that notified it
+    /// It can then access the subjects properties and choose which one to
+    /// display in the interface
+    /// </summary>
+    /// <param name="subject"></param>
+    public override void Notify(Subject subject)
+    {
+        //throw new System.NotImplementedException();
+        if (!_playerController)
+        {
+            _playerController = subject.GetComponent<PlayerController>();
+        }
+
+        if (_playerController)
+        {
+            _isTurboOn = _playerController.IsTurboOn;
+            _currentHealth = _playerController.CurrentHealth;
+        }
+    }
+}
