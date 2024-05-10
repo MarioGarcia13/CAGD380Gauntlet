d32bda3a80eedf7527df408f79c2784b2b07f659
MarioGarcia13 <77987626+MarioGarcia13@users.noreply.github.com>
2024-05-10 15:37:30 -0700

    Door Progress

---
+++ CAGD380_Gauntlet/Assets/_Scripts/Key.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    [Range(1, 20)]
+    public int key = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
